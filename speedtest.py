c5b776ea5592c421fd442d168cf4c260bb0c200f
miclovich <vicmiclovich@gmail.com>
2011-10-11 14:29:10 +0300

    adjustments

--- speedtest.py
+++ speedtest.py
@@ -1,23 +1,29 @@
-# testing through put for message sending in kannel
 # RapidSMS messaging
+# testing through put for message sending in kannel
 
-import timeit
-from kannel import SmsSender
+from kannel import SmsSender, SmsReceiver
+import random
 
-sender = SmsSender("kannel","kannel")
-sender.send("1234566","hello")
+def send_some_texts(dest="12346"):
+    sender = SmsSender(username="kannel", password="kannel")
+    sender.send(dest.strip(), "Test message")
+
+
+# you can run this test using a live modem and if you've got $$ to spend
+def send_to_number_of_users(population=100,elements=10):
+    # choose a population for random function
+    # choose the numbers you want returned, elements.
+    if elements <= population:
+        destination_numbers = random.sample(xrange(population),elements)
+        for num in destination_numbers:
+            send_some_texts(dest=str(num))
+    else:
+        print "choices should not be more than the population size"
+
+
+
 
 if __name__ == "__main__":
-
-	dest = raw_input("Please enter a phone number to receive SMS: ").strip()
-	sender = SmsSender(username="user", password="pass")
-	sender.send(dest, "Test message")
-
-	class TestReceiver():
-		def iGotAnSMS(self, caller, msg):
-			msg = "%s says: %s" % (caller, msg)
-			sender.send(dest, msg)
-
-	tr = TestReceiver()
-	print "Waiting for incomming SMS..."
-	SmsReceiver(tr.iGotAnSMS).run()
+    from timeit import Timer        
+    t = Timer("send_some_texts()", "from __main__ import send_some_texts")
+    print t.timeit()
