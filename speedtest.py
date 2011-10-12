7af35366912fb1dc6edad5f6884e4bb75f2bfaa6
miclovich <vicmiclovich@gmail.com>
2011-10-12 12:55:49 +0300

    updated test code

--- speedtest.py
+++ speedtest.py
@@ -30,14 +30,24 @@ def send_text_in_chunks(number_of_users=100, chunks_of=10):
     index = 0
     num_base=8675309
     num_chunks_lists = chunkify(number_of_users,chunks_of)
+    total_times = []
     for list_base in num_chunks_lists:
+        t1 = time.time()
         send_text(num_base,number_of_users,list_base)
+        t2 = time.time()
+        total_times.append(t2-t1)
+        
+    print "#################################################"
+    print "Sent a total of %d messages in chunks of %d : Max time<%f>, Min time<%f>"%(number_of_users,chunks_of,max(total_times),min(total_times))
 
 
 if __name__ == "__main__":
-    print chunkify(200,10)
+    # sending default (100 messages in chunks of 10)
     send_text_in_chunks()
-#    send_text()
+
+    # sending 1000 messages in chunks of 100
+    send_text_in_chunks(number_of_users=1000,chunks_of=100)
+
 #    for i in STRESS_TEST_RANGE:
 #        t1 = time.time()
 #        send_text(num=i)
