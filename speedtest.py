7225071391c654c89c8ada88133bf0a1c9f24b7c
miclovich <vicmiclovich@gmail.com>
2011-10-12 08:55:40 +0300

    function playing nice

--- speedtest.py
+++ speedtest.py
@@ -1,18 +1,23 @@
 # RapidSMS messaging
 # testing through put for message sending in kannel
+import urllib
 
-from kannel import SmsSender, SmsReceiver
-
-url = "http://localhost:13013/cgi-bin/sendsms?from=1234&username=kannel&password=kannel&to=%s&smsc=dmark&text=hello+world"
 num_base = 8675309
-url = url % "+".join([str(num_base + i) for i in range (0,1000)])
-print url
 
-sender = SmsSender(username="kannel",password="kannel")
-sender.send("8675309","hello world")
-print "message sent"
+STRESS_TEST_RANGE = (10, 25, 100, 1000, 2000, 10000, 50000, 75000, 100000)
 
-#if __name__ == "__main__":
-    #from timeit import Timer
-    #t = Timer("send_some_texts()", "from __main__ import send_some_texts")
-    #print t.timeit()
+TIME = []
+
+def send_text(num=10):
+    url="http://localhost:13013/cgi-bin/sendsms?from=1234&username=kannel&password=kannel&to=%s&smsc=dmark&text=hello+world"
+    url = url%"+".join([str(num_base+i) for i in range(0,num)])
+    f = urllib.urlopen(url)
+    f.read()
+
+if __name__ == "__main__":
+    import time
+    for i in STRESS_TEST_RANGE:
+        t1 = time.time()
+        send_text(num=i)
+        t2 = time.time()
+        print "done sending %d messages %f"% (i,t2-t1)
